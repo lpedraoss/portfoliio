dc21424c1b32d9747a8ba98bb17ac5ab6d99888d
lpedraoss <alfonso.suarez.dev@gmail.com>
2023-07-08 09:27:22 -0500

    fixed:     - the name of image projects

--- src/component/pure/card_project.tsx
+++ src/component/pure/card_project.tsx
@@ -19,15 +19,15 @@ function CardProjects() {
 
   const getImageByProject = (project: Project) => {
     switch (project.imagen) {
-      case 'go-buy.png':
+      case 'go-buy':
         return 'https://firebasestorage.googleapis.com/v0/b/portfolio-3ce56.appspot.com/o/projects%2Fgo-buy.png?alt=media&token=69d58e37-6b76-464a-9042-d838581f7a6b';
-      case 'medical.png':
+      case 'medical':
         return 'https://firebasestorage.googleapis.com/v0/b/portfolio-3ce56.appspot.com/o/projects%2Fmedical.png?alt=media&token=7da1e430-886e-4fcd-921f-5d0e4ccf319a';
-      case 'hackaton.png':
+      case 'hackaton':
         return 'https://firebasestorage.googleapis.com/v0/b/portfolio-3ce56.appspot.com/o/projects%2Fhackaton.png?alt=media&token=bf98d55c-0f31-4ac4-9b82-0e72d7a59a25';
-      case 'spring.png':
+      case 'spring':
         return 'https://firebasestorage.googleapis.com/v0/b/portfolio-3ce56.appspot.com/o/projects%2Fspring.png?alt=media&token=367d15df-bc31-4e59-a909-2adf7ac291fb';
-      case 'dart.png':
+      case 'dart':
         return 'https://firebasestorage.googleapis.com/v0/b/portfolio-3ce56.appspot.com/o/projects%2Fdart.png?alt=media&token=e8bb717d-ade9-4919-b8ba-2b45592d3ea0';
     
     }
