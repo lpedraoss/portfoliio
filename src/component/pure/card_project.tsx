e4784d6d4b58d194ca3f510ce6b9720784cda22c
lpedraoss <alfonso.suarez.dev@gmail.com>
2023-07-07 19:41:56 -0500

    modified:     - card

--- src/component/pure/card_project.tsx
+++ src/component/pure/card_project.tsx
@@ -1,55 +1,30 @@
-import  { useRef } from 'react';
+
 import projects from '../../data/project.json';
 import Project from '../../interface/project_interface';
 
 
 function CardProjects() {
-  const projectList = projects as Project[];
-  const cintaRef = useRef<HTMLDivElement>(null);
+    const projectList = projects as Project[];
 
-  const scrollLeft = () => {
-    if (cintaRef.current) {
-      cintaRef.current.scrollBy({
-        top: 0,
-        left: -320, // Ancho de la tarjeta + margen derecho
-        behavior: 'smooth',
-      });
-    }
-  };
-
-  const scrollRight = () => {
-    if (cintaRef.current) {
-      cintaRef.current.scrollBy({
-        top: 0,
-        left: 320, // Ancho de la tarjeta + margen derecho
-        behavior: 'smooth',
-      });
-    }
-  };
-
-  return (
-    <div className="cinta-horizontal" ref={cintaRef}>
-      {projectList.map((project: Project, index: number) => (
-        <a key={index} href={project.url} target="_blank" rel="noopener noreferrer">
-          <article className="card">
-            <picture>
-              <img src={project.imagen} alt={project.nombre} />
-            </picture>
-            <div>
-              <span>{project.nombre}</span>
-            </div>
-          </article>
-        </a>
-      ))}
-      <button className="scroll-button" onClick={scrollLeft}>
-        ←
-      </button>
-      <button className="scroll-button" onClick={scrollRight}>
-        →
-      </button>
-    </div>
-  );
-}
-
-export default CardProjects;
-
+  
+    return (
+      <section
+        className='card-container'
+      >
+        {projectList.map((project: Project, index: number) => (
+          <a key={index} href={project.url} target="_blank" rel="noopener noreferrer">
+            <article className="card">
+              <picture>
+                <img src={project.imagen} alt={project.nombre} />
+              </picture>
+              <div>
+                <span>{project.nombre}</span>
+              </div>
+            </article>
+          </a>
+        ))}
+      </section>
+    );
+  }
+  
+  export default CardProjects;
