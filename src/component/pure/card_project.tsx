36e4c5d77bd0edaafa5c8fe68885d8ff3c5d5e46
lpedraoss <alfonso.suarez.dev@gmail.com>
2023-07-07 23:31:30 -0500

    modified:     - card

--- src/component/pure/card_project.tsx
+++ src/component/pure/card_project.tsx
@@ -1,30 +1,41 @@
-
+import  { useState, useEffect } from 'react';
 import projects from '../../data/project.json';
 import Project from '../../interface/project_interface';
 
-
 function CardProjects() {
-    const projectList = projects as Project[];
+  const [projectList, setProjectList] = useState<Project[]>([]);
 
-  
-    return (
-      <section
-        className='card-container'
-      >
-        {projectList.map((project: Project, index: number) => (
-          <a key={index} href={project.url} target="_blank" rel="noopener noreferrer">
-            <article className="card">
-              <picture>
-                <img src={project.imagen} alt={project.nombre} />
-              </picture>
-              <div>
-                <span>{project.nombre}</span>
-              </div>
-            </article>
-          </a>
-        ))}
-      </section>
-    );
-  }
-  
-  export default CardProjects;
+  useEffect(() => {
+    // Cargar las imágenes de los proyectos
+    const loadImages = async () => {
+      const updatedProjects = await Promise.all(
+        projects.map(async (project: Project) => {
+          const image = await import(`${project.imagen}`);
+          return { ...project, imagen: image.default };
+        })
+      );
+      setProjectList(updatedProjects);
+    };
+
+    loadImages();
+  }, []);
+
+  return (
+    <section className='card-container'>
+      {projectList.map((project: Project, index: number) => (
+        <a key={index} href={project.url} target="_blank" rel="noopener noreferrer">
+          <article className="card">
+            <picture>
+              <img src={project.imagen} alt={project.nombre} />
+            </picture>
+            <div>
+              <span>{project.nombre}</span>
+            </div>
+          </article>
+        </a>
+      ))}
+    </section>
+  );
+}
+
+export default CardProjects;
